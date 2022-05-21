9380b6d4e41019d9d0324564cc62e2d943e8ccbe
nam <namsupermen06@gmail.com>
2022-05-21 16:40:12 +0700

    css top header

---
+++ src/fakeData.js
@@ -0,0 +1,7 @@
+export const languageFlag = [
+    {
+        id: 1,
+        name: 'English',
+        flag: require('../src/assets/images/flags/uk.png').default,
+    },
+]
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,9 +2,5 @@ import React from "react";
 import Header from "../components/header/Header";
 
 export default function Home() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  return <Header />;
 }
